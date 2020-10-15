25b0e12054cae928b7e95adf464325408064c2e8
henry eze <henryeze019@gmail.com>
2020-10-15 16:51:27 +0100

    sinon added

--- tests/unit/dashboard/addproperty.spec.ts
+++ tests/unit/dashboard/addproperty.spec.ts
@@ -1,9 +1,10 @@
 import addproperty from "@/views/dashboard/addproperty.vue";
 import { shallowMount } from "@vue/test-utils";
 import { expect } from "chai";
+import sinon from "sinon"
 describe("addproperty", () => {
   const wrapper = shallowMount(addproperty, {
-    data: () => {
+    data: () => {  
       return {
         body: {
           type: "",
@@ -14,26 +15,38 @@ describe("addproperty", () => {
           state: "",
           landmark: "",
           images: [],
-          price: 0,
-          bargain: false
+          price: 0
         },
         disabled: true,
         loading: false
       };
     }
   });
-  it("button should be disabled when all compulsory fields are not filled", () => {
-    if (
-      wrapper.vm.$data.body.type.length === 0 ||
-      wrapper.vm.$data.body.size.length === 0 ||
-      wrapper.vm.$data.body.images.length === 0
-    )
+  const spy = sinon.spy();
+  it("button should be disabled when all compulsory fields are not filled", async () => {
       expect(wrapper.vm.$data.disabled).to.equal(true);
-    else expect(wrapper.vm.$data.disabled).to.equal(false);
   });
-  it("form submit", async () => {
-    console.log(wrapper.find(".add__button"))
-    await wrapper.find(".add__button").trigger("click");
+  it("button should be abled when all compulsory fields are filled", async () => {
+    await wrapper.setData({
+      body: {
+        type: "duplex",
+        size: "40 acres",
+        noOfRooms: 70,
+        extra: "just a bit of information",
+        location: "nsukka",
+        state: "enugu,nsukka",
+        landmark: "unn",
+        images: [1, 2, 3, 4],
+        price: 0,
+      }
+    })
+      expect(wrapper.vm.$data.disabled).to.equal(false);
+  });
+  it("Check variables been passed in", async () => {
+    wrapper.find("button.add__button").trigger("click");
+    const clickSpy = sinon.spy(addproperty.addProperty)
+
+    console.log(wrapper.vm.$data.disabled)
     expect(wrapper.vm.$data.disabled).to.equal(true);
     expect(wrapper.vm.$data.loading).to.equal(true);
     expect(typeof wrapper.vm.$data.body.noOfRooms).to.equal(typeof 0);
