660bf07077c286c66c8af7da9a277ef25d61efc9
godofjs <henryeze019@gmail.com>
2020-11-04 17:09:48 +0000

    view owner started

--- src/store/modules/properties.ts
+++ src/store/modules/properties.ts
@@ -20,6 +20,10 @@ export default class Properties extends VuexModule {
     this.property = data;
   }
   @Mutation
+  setOwner(data: Property[]) {
+    this.property = data;
+  }
+  @Mutation
   setUniversalProperties(data: Property[]) {
     this.universalProperty = data;
   }
@@ -46,6 +50,18 @@ export default class Properties extends VuexModule {
       .then(response => {
         console.log(response)
         this.setUniversalProperties(response.data.data);
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
+  @Action
+  getPropertyOwner(id: string) {
+    Api()
+      .get("/api/v1/users/get-user/" + id)
+      .then(response => {
+        console.log(response)
+        this.setOwner(response.data.data);
       })
       .catch(err => {
         throw err;
--- src/views/home/propowner.vue
+++ src/views/home/propowner.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="pt-12 mt-12">
+  <div class="pt-12 mt-8 pa-6">
     <div class="font__purple font__sm font__bold d-flex align-center">
       <span> Owner's Details </span>
     </div>
   </div>
 </template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+import PropertiesModule from "@/store/modules/properties";
+import { getModule } from "vuex-module-decorators";
+@Component({
+  name: "PropOwnerDetails"
+})
+export default class PropOwnerDetails extends Vue {
+  properties = getModule(PropertiesModule, this.$store);
+ mounted() {
+     this.properties.getPropertyOwner(this.$route.params.id)
+ }
+}
+</script>
